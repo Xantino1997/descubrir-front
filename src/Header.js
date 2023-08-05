a6ade53514e5251d67524786be56f678743cf9eb
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-08-04 21:29:56 -0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -8,6 +8,7 @@ const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado para controlar el inicio de sesión
   const [showProfileImg, setShowProfileImg] = useState(false); // Estado para controlar la imagen de perfil
   const [activeLink, setActiveLink] = useState("inicio"); // Estado para el enlace activo
+  const [showMobileMenu, setShowMobileMenu] = useState(false); // Estado para controlar el menú hamburguesa en dispositivos móviles
 
   // Función para manejar el inicio de sesión y mostrar la imagen de perfil
   const handleLogin = () => {
@@ -26,30 +27,45 @@ const Header = () => {
   const handleSetActiveLink = (link) => {
     setActiveLink(link);
   };
+
+  // Función para mostrar u ocultar el menú hamburguesa
+  const toggleMobileMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
+
   return (
     <header className="header">
       <div className="logo">
         <img src={DigitalLogo} alt="Logo de la empresa" />
       </div>
-      <nav className="menu">
+      <nav className={`menu ${showMobileMenu ? "show-mobile-menu" : ""}`}>
         <ul>
-          <li className={activeLink === "inicio" ? "active" : ""}>
+          <li
+            className={activeLink === "inicio" && !showMobileMenu ? "active" : ""}
+            style={{ display: showMobileMenu ? "none" : "block" }}
+          >
             <Link to="/" onClick={() => handleSetActiveLink("inicio")}>
               Inicio
             </Link>
           </li>
-          <li className={activeLink === "nosotros" ? "active" : ""}>
+          <li
+            className={activeLink === "nosotros" && !showMobileMenu ? "active" : ""}
+            style={{ display: showMobileMenu ? "none" : "block" }}
+          >
             <Link to="/about" onClick={() => handleSetActiveLink("nosotros")}>
               Nosotros
             </Link>
           </li>
-          <li className={activeLink === "blog" ? "active" : ""}>
+          <li
+            className={activeLink === "blog" && !showMobileMenu ? "active" : ""}
+            style={{ display: showMobileMenu ? "none" : "block" }}
+          >
             <Link to="/post" onClick={() => handleSetActiveLink("blog")}>
               Blog
             </Link>
           </li>
-          {isLoggedIn && (
-            // Si ha iniciado sesión, muestra estos enlaces adicionales
+          {isLoggedIn && !showMobileMenu && (
+            // Si ha iniciado sesión y no se muestra el menú hamburguesa, muestra estos enlaces adicionales
             <>
               <li className={activeLink === "crear-post" ? "active" : ""}>
                 <Link
@@ -88,7 +104,11 @@ const Header = () => {
           <Link to="/sign-in">Iniciar Sesión</Link>
         )}
       </div>
+      <div className="mobile-menu-button" onClick={toggleMobileMenu}>
+        {showMobileMenu ? "Menu" : "Cerrar"}
+      </div>
     </header>
+    
   );
 };
 
