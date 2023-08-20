efc432ada2b7cb8c2ecfaa5a1a6e8c6afc19bdfb
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-08-20 14:40:34 -0300

    Update IndexPage.js

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "../StylesPages/IndexPage.css";
 import DigitalVideo from "../assets/videopc.mp4";
+import Descubrir from "../assets/pc de fondo 1.avif";
+import Descubrir2 from "../assets/contacto.avif";
+import Descubrir3 from "../assets/Descubrir.png";
 import Swal from "sweetalert2"; // Importa SweetAlert
 
 const IndexPage = () => {
   const [currentWord, setCurrentWord] = useState(0);
   const [showModal, setShowModal] = useState(true); // Estado para mostrar/ocultar el modal
   const [email, setEmail] = useState(""); // Estado para el correo del formulario
-  const titleHome = ["Tecnologia...", "Innovación...", "Creatividad..."];
-
+const titleHome = [/* "Tecnologia...", "Innovación...", "Creatividad..."*/];
+  
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentWord((prevWord) => (prevWord + 1) % titleHome.length);
@@ -67,17 +70,32 @@ const IndexPage = () => {
           </div>
         </div>
       )} */}
-      <div className="background"></div>
+      <div className=""></div>
 
       {/* <div className="brand">
       </div> */}
       <h1 className="title-home">{titleHome[currentWord]}</h1>
+
       <video autoPlay loop muted className="video-bg">
         <source src={DigitalVideo} type="video/mp4" />
         <source src={DigitalVideo} type="video/webm" />
         <source src={DigitalVideo} type="video/ogg" />
         {/* Agrega otros formatos de video para compatibilidad con diferentes navegadores */}
       </video>
+      <div className="diseños-container">
+        <div className="diseños-rutas1">
+          <h1 className="diseños-h1">Desarrollo</h1>
+          <img src={Descubrir2} className="img-desarrollo" alt="" />
+        </div>
+        <div className="diseños-rutas2">
+          <h1 className="diseños-h1">Diseño</h1>
+          <img src={Descubrir} className="img-desarrollo" alt="" />
+        </div>
+        <div className="diseños-rutas3">
+          <h1 className="diseños-h1">Informacion</h1>
+          <img src={Descubrir3} className="img-desarrollo" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
