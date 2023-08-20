36d1f81be54258c7f8958907e3c291b9d4340813
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-08-20 20:45:56 -0300

    Update IndexPage.js

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,277 +1,103 @@
-/* Estilos para la vista de escritorio (PC) */
-@import url("https://fonts.googleapis.com/css2?family=Amatic+SC:wght@700&display=swap");
+import React, { useState, useEffect } from "react";
+import "../StylesPages/IndexPage.css";
+import DigitalVideo from "../assets/videopc.mp4";
+import Descubrir from "../assets/pc de fondo 1.avif";
+import Descubrir2 from "../assets/contacto.avif";
+import Descubrir3 from "../assets/Descubrir.png";
+import Swal from "sweetalert2"; // Importa SweetAlert
 
-.home-container {
-  position: relative;
-  height: 100vh;
-  overflow: hidden;
-}
-.diseños-container {
-  z-index: 60;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  font-family: "Amatic SC", cursive;
-  display: flex;
-  justify-content: space-around;
-  background: transparent;
-}
+const IndexPage = () => {
+  const [currentWord, setCurrentWord] = useState(0);
+  const [showModal, setShowModal] = useState(true); // Estado para mostrar/ocultar el modal
+  const [email, setEmail] = useState(""); // Estado para el correo del formulario
+const titleHome = [/* "Tecnologia...", "Innovación...", "Creatividad..."*/];
+  
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentWord((prevWord) => (prevWord + 1) % titleHome.length);
+    }, 5000); // Cambiar cada 5 segundos
 
-.img-desarrollo {
-  width: 20rem; /* Ancho en rem */
-  height: 15rem; /* Alto en rem */
-  background-size: cover;
-  backdrop-filter: blur(500px); /* Agrega desenfoque al fondo */
-  background-color: rgba(255, 255, 255, 0.3);
-}
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
-.diseños-rutas1 {
+  // Función para manejar el envío del formulario
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes realizar la lógica para enviar el correo y suscribir al usuario al newsletter
 
-  text-align: center;
-  height: 50%;
-  top: 50%;
-  padding: 10px;
-  margin: 15px;
-  margin-top: 100px;
-  cursor: pointer;
-  background: rgba(0, 0, 0, 0.5);
+    // Muestra un SweetAlert de éxito
+    Swal.fire({
+      title: "¡Gracias por suscribirte!",
+      text: "Ahora recibirás las últimas noticias e información digital.",
+      icon: "success",
+      confirmButtonText: "Aceptar",
+    });
 
-}
-.diseños-rutas2 {
+    // Cierra el modal
+    setShowModal(false);
+  };
 
-  text-align: center;
-  height: 50%;
-  top: 50%;
-  padding: 10px;
-  margin: 15px;
-  margin-top: 100px;
-  cursor: pointer;
-  background: rgba(0, 0, 0, 0.5);
+  // Función para manejar el clic en "No por el momento"
+  const handleNoButtonClick = () => {
+    // Cierra el modal
+    setShowModal(false);
+  };
 
-}
-.diseños-rutas3 {
-  text-align: center;
-  height: 50%;
-  top: 50%;
-  padding: 10px;
-  margin: 15px;
-  margin-top: 100px;
-  cursor: pointer;
-  background: rgba(00, 00, 00, 0.5);
+  return (
+    <div className="home-container">
+      {/* {showModal && (
+        // Modal para suscribirse al newsletter
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>¿Quieres suscribirte al newsletter?</h2>
+            <p>Recibe las últimas noticias e información digital en tu correo.</p>
+            <div className="columna">
+              <h3>Suscríbete al Newsletter</h3>
+              <form className="form-modal" onSubmit={handleFormSubmit}>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Ingresa tu correo electrónico"
+                  required
+                />
+                <button type="submit">Suscribirse</button>
+              </form>
+            </div>
+            <button onClick={handleNoButtonClick}>No por el momento</button>
+          </div>
+        </div>
+      )} */}
+      <div className=""></div>
 
-}
+      {/* <div className="brand">
+      </div> */}
+      <h1 className="title-home">{titleHome[currentWord]}</h1>
 
-.diseños-rutas1 {
-  animation-delay: 0s; /* Retraso inicial */
-}
+      <video autoPlay loop muted className="video-bg">
+        <source src={DigitalVideo} type="video/mp4" />
+        <source src={DigitalVideo} type="video/webm" />
+        <source src={DigitalVideo} type="video/ogg" />
+        {/* Agrega otros formatos de video para compatibilidad con diferentes navegadores */}
+      </video>
+      <div className="diseños-container">
+        <div className="diseños-rutas1">
+          <h1 className="diseños-h1">Desarrollo</h1>
+          <img src={Descubrir2} className="img-desarrollo" alt="" />
+        </div>
+        <div className="diseños-rutas2">
+          <h1 className="diseños-h1">Diseño</h1>
+          <img src={Descubrir} className="img-desarrollo" alt="" />
+        </div>
+        <div className="diseños-rutas3">
+          <h1 className="diseños-h1">Informacion</h1>
+          <img src={Descubrir3} className="img-desarrollo" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-.diseños-rutas2 {
-  animation-delay: 1s; /* Retraso para crear un desfase */
-}
-
-.diseños-rutas3 {
-  animation-delay: 2s; /* Retraso para crear un desfase */
-}
-
-@keyframes electric-border {
-  0% {
-    border-color: lightgreen; /* Color inicial */
-    background: transparent; /* Color inicial */
-  }
-  33% {
-    border-color: blue; /* Primer cambio de color */
-    background: rgba(255, 255, 255, 0.1);
-  }
-  66% {
-    border-color: orange; /* Segundo cambio de color */
-    background: rgba(0, 0, 0, 0.7); 
-  }
-  100% {
-    border-color: lightgreen; /* Volver al color inicial */
-    background: rgba(255, 255, 255, 0.1);
-  }
-}
-.diseños-h1 {
-  color: #fff !important;
-  z-index: 66;
-  font-size: 35px;
-  font-weight: bold;
-}
-.background {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: #121;
-  opacity: 0.8;
-  z-index: 1;
-}
-
-.brand {
-  position: absolute;
-  left: 0;
-  top: 0;
-  border: #000 solid 2px;
-  padding-left: 10px;
-  padding-right: 10px;
-  width: 30%;
-  height: 99.5%;
-  background-image: url("../assets/DIGITAL.png");
-  background-size: cover;
-  background-position: center;
-  z-index: 2;
-}
-
-.title-home {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 48px;
-  color: #fff !important;
-  text-align: center;
-  z-index: 3;
-}
-
-.video-bg {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  z-index: 0;
-}
-
-.form-modal {
-  display: flex;
-  text-align: center;
-  margin: 30px;
-}
-
-.form-modal input {
-  display: flex;
-  text-align: center;
-  margin: 30px;
-}
-
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.2);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 55;
-}
-
-.modal {
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 8px;
-  max-width: 400px;
-  text-align: center;
-}
-
-.modal h2 {
-  font-size: 24px;
-  margin-bottom: 10px;
-}
-
-.modal p {
-  font-size: 16px;
-  margin-bottom: 20px;
-}
-
-.modal button {
-  background-color: #d60000;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  font-size: 16px;
-  cursor: pointer;
-  margin-right: 10px;
-  border-radius: 5px;
-}
-
-.modal button.secondary {
-  background-color: #555;
-}
-
-.modal button:hover {
-  background-color: #d60000;
-}
-
-/* Estilos para la vista de dispositivos móviles (celulares) */
-@media (max-width: 768px) {
-  .title-home {
-    font-size: 36px;
-    top: 35%;
-  }
-
-  .brand {
-    left: 0;
-    right: 0;
-    top: 100px;
-    margin: 0 auto;
-    width: 70%;
-    height: 70px;
-    background-image: url("../assets/DIGITAL.png");
-    background-size: contain;
-    background-position: center;
-    background-repeat: no-repeat;
-    border: none; /* Eliminamos el borde en dispositivos móviles */
-    padding: 0; /* Eliminamos el padding en dispositivos móviles */
-  }
-
-  .diseños-container {
-    z-index: 90;
-    position: absolute;
-    width: 100%;
-    top:10rem;
-    height: 50%;
-    text-align: center;
-    font-family: "Amatic SC", cursive;
-    display: flex;
-    flex-direction: column; /* Cambiamos la dirección del flujo a vertical */
-    align-items: center; /* Centramos horizontalmente */
-    background: transparent;
-  }
-
-  .img-desarrollo {
-    width: 50%; /* Ancho del 100% para ocupar todo el ancho disponible */
-    height: 30%; /* Altura automática para mantener la proporción */
-    background-size: cover;
-    backdrop-filter: blur(5px); /* Agrega un ligero desenfoque al fondo */
-    background-color: rgba(255, 255, 255, 0.3);
-  }
-
-  .diseños-rutas1,
-  .diseños-rutas2,
-  .diseños-rutas3 {
-    text-align: center;
-    height: 50%;
-    width:50%;
-    padding: 10px;
-    margin: 15px;
-    cursor: pointer;
-    background: rgba(0, 0, 0, 0.5);
-  }
-
-  .diseños-rutas1 {
-    margin-top: 10px; /* Ajustamos el margen superior */
-  }
-
-  .diseños-rutas2 {
-    margin-top: 10px; /* Ajustamos el margen superior */
-  }
-
-  .diseños-rutas3 {
-    margin-top: 10px; /* Ajustamos el margen superior */
-  }
-}
+export default IndexPage;
