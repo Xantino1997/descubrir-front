9ae90c6a29a544e9bdde9ea2239d2c5d0f7660ad
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-08-20 20:38:00 -0300

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,134 +1,128 @@
-import React, { useState } from "react";
-import "./StylesPages/Footer.css";
-import Swal from "sweetalert2";
-import { Link } from "react-router-dom";
-import Facebook from "./assets/facebook.png";
-import Instagram from "./assets/instagram.png";
-import WhatsApp from "./assets/whatsapp.png";
+/* Estilos del footer para la vista de escritorio (PC) */
+.footer {
+  background-color: #225;
+  color: #fff;
+  padding: 40px;
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-around;
+  background-image: url('../assets/header.jpg'); /* Ruta a tu archivo SVG */
+}
 
-const Footer = () => {
-  const [email, setEmail] = useState("");
-  const currentYear = new Date().getFullYear();
-  const phoneNumber = "543465650158"; // Reemplaza con el número de teléfono correcto
+.columna {
+  flex: 1;
+  margin-right: 20px;
+  text-align: center;
+}
 
-  const handleWhatsAppClick = () => {
-    const message = "Hola, quiero saber más de este producto.";
+.columna:last-child {
+  margin-right: 0;
+}
 
-    if (
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-      )
-    ) {
-      // Dispositivo móvil
-      window.open(
-        `whatsapp://send?phone=${phoneNumber}&text=${encodeURIComponent(
-          message
-        )}`,
-        "_blank"
-      );
-    } else {
-      // Computadora de escritorio
-      window.open(
-        `https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
-          message
-        )}`,
-        "_blank"
-      );
-    }
-  };
+.form-footer {
+  display: flex;
+  text-align: center;
+  margin-top: 30px;
+}
 
-  const handleSubmit = async (e) => {
-    // ...el resto del código para el envío de suscripción
-  };
+.form-footer input {
+  text-align: center;
+}
 
-  return (
-    <>
-      <footer className="footer">
-        <div className="columna">
-          <h3>Redes</h3>
-          <ul>
-            <li>
-              <a
-                href="https://www.facebook.com/profile.php?id=100095504168154"
-                target="_blank" // Agregamos target="_blank" para que se abra en una nueva ventana
-                rel="noopener noreferrer"
-              >
-                <img className="redes-sociales" src={Facebook} alt="Facebook" />
-                Facebook
-              </a>
-            </li>
-            <li>
-              <Link to="/instagram">
-                <img
-                  className="redes-sociales"
-                  src={Instagram}
-                  alt="Instagram"
-                />
-                Instagram
-              </Link>
-            </li>
-            <li>
-              <a
-                href="#"
-                onClick={handleWhatsAppClick}
-                rel="noopener noreferrer"
-              >
-                <img className="redes-sociales" src={WhatsApp} alt="WhatsApp" />
-                WhatsApp
-              </a>
-            </li>
-          </ul>
-        </div>
-        {/* <div className="columna">
-          <h3>Contacto</h3>
-          <ul>
-            <li>
-              <a href="#">Contacto 1</a>
-            </li>
-            <li>
-              <a href="#">Contacto 2</a>
-            </li>
-            <li>
-              <a href="#">Contacto 3</a>
-            </li>
-          </ul>
-        </div> */}
-        {/* <div className="columna">
-          <h3>Empresa</h3>
-          <ul>
-            <li>
-              <a href="#">Enlace 1</a>
-            </li>
-            <li>
-              <a href="#">Enlace 2</a>
-            </li>
-            <li>
-              <a href="#">Enlace 3</a>
-            </li>
-          </ul>
-        </div> */}
-        <div className="columna">
-          <h3>Suscríbete al Newsletter</h3>
-          <form className="form-footer" onSubmit={handleSubmit}>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Ingresa tu correo electrónico"
-              required
-            />
-            <button type="submit">Suscribirse</button>
-          </form>
-        </div>
-      </footer>
-      <div className="copyright">
-        <Link to="/terminos">
-          Descubrir Digital. Todos los derechos reservados.
-        </Link>
-        <p>&copy; {currentYear}</p>
-      </div>
-    </>
-  );
-};
+h3 {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
 
-export default Footer;
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  margin-bottom: 5px;
+}
+
+a {
+  text-decoration: none;
+  color: #fff;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+.columna form {
+  display: flex;
+  flex-direction: column;
+}
+
+.columna input {
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.columna button {
+  padding: 10px;
+  background-color: #fc2003;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.columna button:hover {
+  background-color: #d60b00;
+}
+
+.redes-sociales {
+  width: 30px;
+  border-radius: 25px;
+  margin-right: 5px;
+
+
+}
+
+.copyright {
+  width: 100%;
+  text-align: center;
+  background-color: #111;
+  color: #fff;
+  padding-top: 10px;
+}
+.terminos {
+  color: #000;
+}
+/* Estilos del footer para la vista de dispositivos móviles (celulares) */
+@media (max-width: 768px) {
+  .footer {
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    background-image: url("../assets/header.jpg"); /* Ruta a tu archivo SVG */
+  }
+  a {
+    text-decoration: none;
+    color:red ;
+    font-weight: bold ;
+  }
+  .columna {
+    margin-right: 0;
+    margin-bottom: 20px;
+
+  }
+
+  .form-footer {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .columna form {
+    align-items: center;
+  }
+}
