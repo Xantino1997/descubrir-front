c6709f6d0227d65f82487b7ce07547838c315348
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-12-11 10:53:57 -0300

    add DIGITAL.png

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Fade } from "react-awesome-reveal"; // Importa Fade desde react-awesome-reveal
-import Digital from "../assets/DIGITAL.avif";
+import Digital from "../assets/DIGITAL.png";
 import TeamDigital from "../assets/Team-digital.png";
 import "../StylesPages/About.css";
 import { Link } from "react-router-dom";
